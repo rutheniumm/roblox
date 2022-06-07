b5ae227fae4ba77c227a0c19d541bfbd6c9e3d3d
rutheniumm <93807418+rutheniumm@users.noreply.github.com>
2022-06-07 17:25:04 -0400

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -4,7 +4,7 @@ local HttpService = game:GetService("HttpService")
 
 setmetatable(Module, {__index = function(tbl, index, value)
 pcall(function()
-return loadstring(HttpService:GetAsync(string.format("https://raw.githubusercontent.com/rutheniumm/roblox/main/%s", index)))()
+return loadstring(HttpService:GetAsync(string.format("https://raw.githubusercontent.com/rutheniumm/roblox/main/%s", index)))();
 end)
 end})  
 
