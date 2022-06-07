3dbc8e7cea37c4daca629ba484dbf176056f7717
rutheniumm <93807418+rutheniumm@users.noreply.github.com>
2022-06-07 17:20:28 -0400

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,11 +1,10 @@
 local Module = {}
 local HttpService = game:GetService("HttpService")
 
-local SubModules = {["FastCastRedux"] = 'https://raw.githubusercontent.com/rutheniumm/roblox/main/FastCastRedux.lua'}
 
 setmetatable(Module, {__index = function(tbl, index, value)
 pcall(function()
-return loadstring(HttpService:GetAsync(SubModules[index]))()
+return loadstring(HttpService:GetAsync(string.format("https://raw.githubusercontent.com/rutheniumm/roblox/main/%s", index))()
 end)
 end})  
 
